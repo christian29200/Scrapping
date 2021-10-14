fd7e846977e1784e34c1c6bfeda3f20b3fe9fa73
MelinaDCB <melina.decombas@gmail.com>
2021-10-14 12:37:09 +0200

    first-ex-done

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -3,24 +3,30 @@ require 'nokogiri'
 require 'open-uri'
 
 
-page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/")) #se connecte à la page
 
+def create_crypto_hash
+  page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/")) #se connecte à la page
 
-def scrap_line(line) #Méthode pour récupérer le nom et la valeur d'une ligne
-  return{ line.css('td').css('div')[2].text.intern => line.css('td').css('div')[3].text.gsub(/[\$,]/, '').to_f }
+  i = 0
+  symbol_list = [] #Naming choice by Melina 
+  value_list = [] # <- naming variables like this actually MAKES SENSE
+  begin
+    loop do
+      i += 1
+      #puts i
+      symbol = page.xpath("//*[@id=\"__next\"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{i}]/td[3]/div")
+        symbol_list << symbol.text
+      value = page.xpath("//*[@id=\"__next\"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr[#{i}]/td[5]/div/a")
+        value_list << value.text
+      if i ==20
+        break
+      end 
+  end
+  rescue => e
+    puts "Oups petite erreur, mais c'est pas grave" #résultat que tu veux voir en lieu et place d'une erreur terminal
+  end
+
+ return Hash[symbol_list.zip(value_list)]
 end
 
-
-a = []
-begin
-page.xpath('//*[@id="__next"]/div[1]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr').each do |line| #Nokogiri elements
-  a << scrap_line(line) #utilise la méthode 'scrap_line' pour chaque ligne et insère le résultat dans l'array 'a'.
-end
-rescue => e
-  puts "Oups petite erreur, mais c'est pas grave" #résultat que tu veux voir en lieu et place d'une erreur terminal
-  puts e
-end
- # Génère un fichier.txt avec les résultats de l'array 'a'.
-puts a
-
-
+puts create_crypto_hash
