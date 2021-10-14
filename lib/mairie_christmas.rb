896e7cd909b054eb439fbae24636f1b146086105
MelinaDCB <melina.decombas@gmail.com>
2021-10-14 15:38:36 +0200

    fourth-commit

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -6,7 +6,7 @@ require 'pry'
 def get_townhall_emails(townhall_url) 
     list_email = []
     list_cities = []
-    page = Nokogiri::HTML(URI.open(townhall_url))
+    page = Nokogiri::HTML(URI.open("#{townhall_url}"))
     email = page.xpath("/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]").text
     city = page.xpath("/html/body/div/main/section[1]/div/div/div/p[1]/strong[1]/a").text
     list_cities << city
@@ -15,16 +15,13 @@ def get_townhall_emails(townhall_url)
     else
         list_email << email
     end
-    puts list_email
-    puts list_cities
+    puts Hash[list_cities.zip(list_email)]
 end
 
-def get_townhall_urls
-    list_urls = []
-    page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
-    link = page.xpath('//a[contains(@href, "./95/")]')
-    puts link
-    list_urls << link
-end
 
-get_townhall_emails("http://annuaire-des-mairies.com/95/grisy-les-platres.html")
+page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
+link = page.xpath('//a[contains(@href, "./95/")]/@href')
+list_urls = link.map {|x| "https://www.annuaire-des-mairies.com" + x.text[1..-1]}
+
+list_urls.each {|x| get_townhall_emails(x)}
+
