e986184f29de5378bbed05d5076dfe98153d1abc
christian29200 <christianmbarga29@yahoo.fr>
2021-10-14 23:54:42 +0200

    Final 2

---
+++ spec/marie_christmas_spec.rb
@@ -0,0 +1,10 @@
+# require_relative '../lib/mairie_christmas'
+
+# describe "A mail should be returned" do
+#     it "should return a email " do
+#         expect(get_townhall_emails.class).to eq(Hash)
+#     end
+#   end
+
+
+  
